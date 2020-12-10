647c95a75a3c9bd8a239b46a1d9c4a05d448be61
Fadheljanuar <62705800+Fadheljanuar@users.noreply.github.com>
2020-12-10 23:39:26 +0700

    Layout

---
+++ app/src/main/java/com/example/ucreation/ui/main/detail/DetailKaryaFragment.java
@@ -0,0 +1,29 @@
+package com.example.ucreation.ui.main.detail;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ucreation.R;
+
+
+public class DetailKaryaFragment extends Fragment {
+
+
+
+    public DetailKaryaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_karya, container, false);
+    }
+}
