d3e489fbee3e09c0734a66a5c4888ae90fd49856
jokev106 <kevinmarsiano@gmail.com>
2020-12-13 22:32:43 +0700

    mvvm tingkat 2
    perkembangan top kesel

--- app/src/main/java/com/example/ucreation/ui/splash/SplashFragment.java
+++ app/src/main/java/com/example/ucreation/ui/splash/SplashFragment.java
@@ -2,16 +2,27 @@ package com.example.ucreation.ui.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ucreation.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class SplashFragment extends Fragment {
+
+    @BindView(R.id.btncreation)
+    Button button;
 
 
     public SplashFragment() {
@@ -24,5 +35,16 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        button.setOnClickListener(v -> {
+            NavDirections action = SplashFragmentDirections.actionSplashFragmentToCreationFragment();
+            Navigation.findNavController(view).navigate(action);
+        });
     }
 }
