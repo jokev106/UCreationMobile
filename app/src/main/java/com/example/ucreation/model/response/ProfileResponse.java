a8300157422f250b71001385a7fbb2e5972f9f9e
ardiankls <ardiankurniawan7@gmail.com>
2021-01-20 23:26:08 +0700

    ternyata masih eror saat dirun boi

--- app/src/main/java/com/example/ucreation/model/response/ProfileResponse.java
+++ app/src/main/java/com/example/ucreation/model/response/ProfileResponse.java
@@ -1,4 +1,16 @@
 package com.example.ucreation.model.response;
 
+import com.example.ucreation.model.local.Profile;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class ProfileResponse {
+
+    @SerializedName("data")
+    private List<Profile> results;
+
+    public List<Profile> getResults() {
+        return results;
+    }
 }
